3d05d1b512c2c77c7977b75db2885af8702ae06f
nawdist <receptivegamer@gmail.com>
2022-06-10 21:57:32 +0100

    Added Logging, Chalker, Improved Loader and added some documentation

--- Logger.cs
+++ Logger.cs
@@ -1,10 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
 using System;
-using System.Threading;
 using System.Text;
-namespace NawLog
+namespace CSLog
 {
-
-    public class Loader
+    public enum LoadingState
+    {
+        Success,
+        Error,
+        Warning,
+        Info,
+    };
+    public class LoadingBar
     {
 
         private readonly char[] chars = { ' ', '█' };
@@ -15,14 +22,11 @@ namespace NawLog
         private int k = 0;
         private bool started = false;
         private int currentLevel;
-        public Loader()
-        {
-
-        }
 
         public void Start()
         {
-            
+            //change console encoding to utf-16
+            Console.OutputEncoding = Encoding.Unicode;
             started = true;
             position[0] = Console.CursorLeft;
             position[1] = Console.CursorTop;
@@ -33,9 +37,6 @@ namespace NawLog
             currentLevel = level;
             if (!started) throw new Exception("Cannot call Update() before Start()");
             Console.SetCursorPosition(position[0], position[1]);
-
-            if(k <= 3) k = 0;
-            else k++; 
 
             bar = new StringBuilder("[");
             for (int j = 1; j < 25; j++)
@@ -49,30 +50,221 @@ namespace NawLog
                 bar[i] = chars[1];
             }
 
+
+            if (k >= 3) k = 0;
+            else k++;
             Console.Write($" {spinner[k]} {bar.ToString()} {level}%");
         }
         public void Update()
         {
+            Console.SetCursorPosition(position[0], position[1]);
+            if (k >= 3) k = 0;
+            else k++;
             Console.Write($" {spinner[k]} {bar.ToString()} {currentLevel}%");
         }
 
+        public void Success(string message)
+        {
+            Console.SetCursorPosition(position[0], position[1]);
+            started = false;
+            Console.WriteLine("✓ ".Green() + message);
+        }
+
+        public void End(string message, LoadingState state)
+        {
+            Console.SetCursorPosition(position[0], position[1]);
+            Clear();
+            started = false;
+            switch (state)
+            {
+                case LoadingState.Success:
+                    Console.WriteLine(("✓ " + message).Green());
+                    break;
+                case LoadingState.Error:
+                    Console.WriteLine(("✗ " + message).Red());
+                    break;
+                case LoadingState.Warning:
+                    Console.WriteLine(("⚠ " + message).Yellow());
+                    break;
+                case LoadingState.Info:
+                    Console.WriteLine(("ⓘ " + message).Cyan());
+                    break;
+            }
+        }
+
+        public void End(string message)
+        {
+            Console.SetCursorPosition(position[0], position[1]);
+            Clear();
+            started = false;
+            Console.WriteLine(message);
+        }
+
+        public void End()
+        {
+            End("");
+        }
+        private void Clear()
+        {
+            started = false;
+            Console.SetCursorPosition(position[0], position[1]);
+            Console.Write(new string(' ', Console.WindowWidth));
+        }
+
+    }
+}
+public class Logger
+{
+    public void Info(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.Write("[INFO ⓘ]");
+        Console.ResetColor();
+        Console.WriteLine(" " + message);
+    }
+    public void Warning(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("[WARNING ⚠]");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(" " + message);
+        Console.ResetColor();
     }
 
-
-    public class Log
+    public void Error(string message)
     {
-        struct Vec2
-        {
-            public Vec2(int _x, int _y)
-            {
-                x = _x;
-                y = _y;
-            }
-            public int x, y;
-        }
-        public static void Info(string message)
-        {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.Write("[ERROR ✗]");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(" " + message);
+        Console.ResetColor();
+    }
 
-        }
+    public void Success(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.Write("[SUCCESS ✓]");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(" " + message);
+        Console.ResetColor();
+    }
+
+    public void Debug(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.DarkGray;
+        Console.Write("[DEBUG]");
+        Console.ResetColor();
+        Console.WriteLine(" " + message);
+    }
+
+}
+public static class Chalker
+{
+    public static string Red(this string message)
+    {
+        return $"\u001b[31m{message}\u001b[0m";
+    }
+    public static string Green(this string message)
+    {
+        return $"\u001b[32m{message}\u001b[0m";
+    }
+    public static string Yellow(this string message)
+    {
+        return $"\u001b[33m{message}\u001b[0m";
+    }
+    public static string Blue(this string message)
+    {
+        return $"\u001b[34m{message}\u001b[0m";
+    }
+    public static string Magenta(this string message)
+    {
+        return $"\u001b[35m{message}\u001b[0m";
+    }
+    public static string Cyan(this string message)
+    {
+        return $"\u001b[36m{message}\u001b[0m";
+    }
+    public static string White(this string message)
+    {
+        return $"\u001b[37m{message}\u001b[0m";
+    }
+    public static string Black(this string message)
+    {
+        return $"\u001b[30m{message}\u001b[0m";
+    }
+    public static string Bold(this string message)
+    {
+        return $"\u001b[1m{message}\u001b[0m";
+    }
+    public static string Underline(this string message)
+    {
+        return $"\u001b[4m{message}\u001b[0m";
+    }
+    public static string Invert(this string message)
+    {
+        return $"\u001b[7m{message}\u001b[0m";
+    }
+    public static string Blink(this string message)
+    {
+        return $"\u001b[5m{message}\u001b[0m";
+    }
+    public static string Hide(this string message)
+    {
+        return $"\u001b[8m{message}\u001b[0m";
+    }
+    public static string Strike(this string message)
+    {
+        return $"\u001b[9m{message}\u001b[0m";
+    }
+    public static string Reset(this string message)
+
+    {
+        return $"\u001b[0m{message}\u001b[0m";
+    }
+
+    public static string Italic(this string message)
+    {
+        return $"\u001b[3m{message}\u001b[0m";
+    }
+
+    public static string BgRed(this string message)
+    {
+        return $"\u001b[41m{message}\u001b[0m";
+    }
+    public static string BgGreen(this string message)
+    {
+        return $"\u001b[42m{message}\u001b[0m";
+    }
+    public static string BgYellow(this string message)
+    {
+        return $"\u001b[43m{message}\u001b[0m";
+    }
+    public static string BgBlue(this string message)
+    {
+        return $"\u001b[44m{message}\u001b[0m";
+    }
+    public static string BgMagenta(this string message)
+    {
+        return $"\u001b[45m{message}\u001b[0m";
+    }
+    public static string BgCyan(this string message)
+    {
+        return $"\u001b[46m{message}\u001b[0m";
+    }
+    public static string BgWhite(this string message)
+    {
+        return $"\u001b[47m{message}\u001b[0m";
+    }
+    public static string BgBlack(this string message)
+    {
+        return $"\u001b[40m{message}\u001b[0m";
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,153 @@
-# NawLog
+# CSLog
 
-Some class i made to practice C#. Use it if you want, i don't recommend to because optimization was *not* on mind
+Some utilites i made to practice C#. Use it if you want, i don't recommend to because optimization was *not* on mind
 
-## How to use
+You can just grab the `Logger.cs` file and it'll do the trick.
+
+Feel free to modify the code to fit your needs, 70% of it was written by Copilot anyways.
 
 If, for god knows why, you chose you to use this, here's a brief overview
 
-### Loading bar:
+- [CSLog](#cslog)
+- [Loading bar](#loading-bar)
+    - [Update](#update)
+    - [End](#end)
+- [Logger](#logger)
+    - [Methods](#methods)
+- [Chalker](#chalker)
+  - [Colors](#colors)
+    - [Foreground colors](#foreground-colors)
+    - [Background colors](#background-colors)
+    - [Additional Styles](#additional-styles)
+
+# Loading bar
 ```cs
-    NawLog.Loader bar = new NawLog.Loader();
-    //Be sure to call the Start() method before using it
-    bar.Start()
+    using CSLog;
+    //...
+    
+        LoadingBar bar = new LoadingBar();
+        bar.Start(); //Be sure to call the Start() method before using any other method
+    //...
 ```
 
-### Logging system
-still not implemented lol
+### Update
+
+```cs
+public string Update (int level);
+```
+
+Set the loading bar to the percentage specified.   
+Examples:
+```cs
+bar.Update(10)
+```
+
+```shell
+>>/ [█                       ] 10%
+```
+---
+```cs
+bar.Update(75)
+```
+```shell
+>>| [██████████████████      ] 75%
+```
+---
+Overload:
+```cs
+public string Update();
+```
+Simply updates the spinner without any changes to the loading bar.
+
+### End
+Overloads:
+```cs
+public void End(string message, LoadingState state);
+``` 
+```cs
+public void End(string message);
+``` 
+```cs
+public void End();
+```
+Terminates the loading bar with an optional message and state.
+
+```cs
+LoadingBar bar = new LoadingBar();
+
+bar.End("Finished task successfully!", LoadingState.Success);
+>> ✓ Finished task successfully!
+
+bar.End("Task failed :/", LoadingState.Error);
+>> ✗ Task Failed :/
+
+bar.End("Outdated version", LoadingState.Warning);
+>> ⚠ Outdated version
+
+bar.End("Outdated version", LoadingState.Info);
+>> ⓘ Installed Version 7.26
+```
+
+# Logger
+Essential methods for logging.
+
+### Methods
+Each of their purpose is pretty self-explanatory
+
+```cs
+public void Info(string message)
+```
+```cs
+public void Error(string message)
+```
+```cs
+public void Success(string message)
+```
+```cs
+public void Warning(string message)
+```
+```cs
+public void Debug(string message)
+```
+
+# Chalker
+String extension methods for colored output
+
+## Colors
+### Foreground colors
+- Red
+- Green
+- Yellow
+- Blue
+- Cyan
+- Magenta
+- White
+- Black
+
+### Background colors
+- BgRed
+- BgGreen
+- BgYellow
+- BgBlue
+- BgCyan
+- BgMagenta
+- BgWhite
+- BgBlack
+
+### Additional Styles
+- Bold
+- Italic
+- Underline
+- Strike
+- Invert
+- Hide
+- Reset
+
+---
+
+Example:
+
+```cs
+string message = "some message";
+Console.WriteLine(message.Bold().Italic().BgRed().Blue());
+```
